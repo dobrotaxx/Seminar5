79ef8122ce5a3c05414f4030faae44e971f96adf
Semyon <tonkih.sem@mail.ru>
2022-10-10 22:25:05 +0500

    added task 32

---
+++ task31/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+Найдите сумму отрицательных и положительных элементов массива.
+Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, 
+сумма отрицательных равна -20.
+*/
+
+int[] getRandomArray(int length, int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+void printArray(int[] incomingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+int getSumOfPositiveNumbersInArray(int[] incomingArray)
+{
+    int result = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        if (incomingArray[i] > 0)
+        {
+            result += incomingArray[i];
+        }
+    }
+    return result;
+}
+int getSumOfNegativeNumbersInArray(int[] incomingArray)
+{
+    int result = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        if (incomingArray[i] < 0)
+        {
+            result += incomingArray[i];
+        }
+    }
+    return result;
+}
+// diviation = 1 если считать положительные и -1 если отрицательные
+int getSumOfPositiveNumbersOrNegativeNumbersInArray(int[] incomingArray, int diviation)
+{
+    int result = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        if (incomingArray[i] * diviation> 0)
+        {
+            result += incomingArray[i];
+        }
+    }
+    return result;
+}
+int[] currentArray = getRandomArray(12, -9, 9);
+printArray(currentArray);
+int sumOfPositive = getSumOfPositiveNumbersInArray(currentArray);
+int sumOgNegative = getSumOfNegativeNumbersInArray(currentArray);
+
+Console.WriteLine($"Сумма положительных чисел = {sumOfPositive}, сумма отрицательных чисел = {sumOgNegative}");
+sumOfPositive = getSumOfPositiveNumbersOrNegativeNumbersInArray(currentArray,1);
+sumOgNegative = getSumOfPositiveNumbersOrNegativeNumbersInArray(currentArray,-1);
+
+Console.WriteLine($"Сумма положительных чисел = {sumOfPositive}, сумма отрицательных чисел = {sumOgNegative}");
---
+++ task32/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 32: Напишите программу замены элементов массива: 
+положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+*/
+
+int[] getRandomArray(int length, int startPoint, int endPoint) //создаем массив с рандомными значениями
+{
+    int[] resultArray = new int[length]; /*новый массив int length - длина массива 
+    int startPoint - число от которого будет рандом и int endPoint - число до которого*/
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1); //заполнаяем массив рандомными значениями
+    }
+    return resultArray; // возвращаем получившийся массив
+}
+void printArray(int[] incomingArray) //печатаем массив
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] reverseArray(int[] incomingArray) // меняем положительные значения на отрицательные и наоборот
+{
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        incomingArray[i] *= (-1); // меняем положительные значения на отрицательные и наоборот 
+    }
+    return incomingArray; // возвращаем получившийся массив
+}
+int[] currentArray = getRandomArray(12, -9, 9);
+printArray(currentArray);
+int [] reverse = reverseArray(currentArray);
+printArray(reverse);
