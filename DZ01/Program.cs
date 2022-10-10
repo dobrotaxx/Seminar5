a6807f6ee7d769ca8bb7213761bb5d37f753be50
Semyon <tonkih.sem@mail.ru>
2022-10-11 03:41:22 +0500

    added DZ01

---
+++ DZ01/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+int getNumberFromUser(string userInformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0")
+        {
+            Console.WriteLine($"Ошибка ввода! Введите целое число: [{userLine}].");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return result;
+}
+
+void printArray(int[] incomingArray) //печатаем массив
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+int[] getRandomArray(int length, int startPoint, int endPoint) //создаем массив с рандомными значениями
+{
+    int[] resultArray = new int[length]; /*новый массив int length - длина массива 
+    int startPoint - число от которого будет рандом и int endPoint - число до которого*/
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1); //заполнаяем массив рандомными значениями
+    }
+    return resultArray; // возвращаем получившийся массив
+}
+int quantityOfEvenNumbers(int [] incomingArray)
+{
+    int result = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        if(incomingArray[i] % 2 == 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+int length = getNumberFromUser("Введите длину массива: ");
+int[] currentArray = getRandomArray(length, 100, 1000);
+printArray(currentArray);
+int result = quantityOfEvenNumbers(currentArray);
+Console.Write($"Число четных чисел в массиве = {result}");
