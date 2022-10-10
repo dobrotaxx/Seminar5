1e5d9f6fbbe042893c323bf3c0104ddc6c2c62c1
Semyon <tonkih.sem@mail.ru>
2022-10-11 00:02:39 +0500

    added task 35

--- task33/Program.cs
+++ task33/Program.cs
@@ -3,3 +3,58 @@
 4; массив [6, 7, 19, 345, 3] -> нет
 -3; массив [6, 7, 19, 345, 3] -> да
 */
+
+void printArray(int[] incomingArray) //печатаем массив
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] getRandomArray(int length, int startPoint, int endPoint) //создаем массив с рандомными значениями
+{
+    int[] resultArray = new int[length]; /*новый массив int length - длина массива 
+    int startPoint - число от которого будет рандом и int endPoint - число до которого*/
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1); //заполнаяем массив рандомными значениями
+    }
+    return resultArray; // возвращаем получившийся массив
+}
+
+void findNumber(int[] incomingArray, int number) //ищем число в массиве
+{
+    int result = 0;
+    for (int i = 0; i < incomingArray.Length; i++)//перебор массива
+    {
+        if (number == incomingArray[i]) // если нужное число равно какому-то элементу массива 
+        {
+            result = 1; // то result равно 1
+            break;// и прерываем дальнейший поиск
+        }
+        else//иначе
+        {
+            result = 0;// result равен 0
+        }
+    }
+    if (result > 0) //если result больше нуля, то
+    {
+        Console.WriteLine("Да");// печатаем "Да"
+    }
+    else
+    {
+        Console.WriteLine("Нет");// если не равен нулю, то печатаем "Нет"
+    }
+}
+
+int[] array = getRandomArray(5, -10, 99);//Кладем в переменную массив длиной 5 элементов с рандомными значениями от -10 до 99
+printArray(array);//печатаем массив
+findNumber(array, 9);//Печатаем результат поиска девятки внутри массива без принудительного добавления элемента
---
+++ task35/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
+значения которых лежат в отрезке [10,99].
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5
+*/
+
+int getNumberFromUser(string userInformation)//берем данные от пользователя и обрабатываем их
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);//пишем пользователю сообщение
+        string userLine = Console.ReadLine();//представляем данные от пользователя строковым типом
+        int.TryParse(userLine, out result);//парсим строковые данные от пользователя в как int-овые в result
+        if (result == 0 && userLine != "0")//если данные не равны 0 и result до сих пор 0...
+        {
+            Console.WriteLine($"Ошибка ввода! Введите целое число: [{userLine}].");//печатаем это
+        }
+    }
+    return result;
+}
+
+void printArray(int[] incomingArray) //печатаем массив
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] getRandomArray(int length, int startPoint, int endPoint) //создаем массив с рандомными значениями
+{
+    int[] resultArray = new int[length]; /*новый массив int length - длина массива 
+    int startPoint - число от которого будет рандом и int endPoint - число до которого*/
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1); //заполнаяем массив рандомными значениями
+    }
+    return resultArray; // возвращаем получившийся массив
+}
+
+int findElements(int[] incomingArray, int startPoint, int endPoint)//ищем элементы в определенном диапазоне внутри массива
+{
+    int result = 0;
+    for (int i = 0; i < incomingArray.Length; i++)//перебор массива
+    {
+        if (incomingArray[i] > startPoint && incomingArray[i] < endPoint)
+        {
+            result += 1;
+        }
+    }
+    return result;
+}
+
+int quantityElements = getNumberFromUser("Задайте количество элементов в массиве: ");
+int[] currentArray = getRandomArray(quantityElements, -99, 199);//присваиваем переменной сгенерированный массив
+printArray(currentArray);//печатаем массив
+int result = findElements(currentArray, 10, 99);//ищем в массиве числа в диапазаоне от 10 до 99
+Console.WriteLine($"Количество элементов в массиве в диапазоне от 10 до 99 = {result}");
+
